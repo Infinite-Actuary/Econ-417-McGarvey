6ebece195da065334fb090bf3533e9f686f1e9d1
Infinite-Actuary <unl.actuary@gmail.com>
2018-08-13 12:58:24 -0500

    creating vuepress site

---
+++ .vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Intro to Econometrics',
+  description: 'ECON 417 McGarvey',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/Infinite-Actuary/Econ-417-McGarvey' }
+    ],
+    sidebar: [
+      '/',
+      '/textbooks/',
+    ]
+  }
+}
